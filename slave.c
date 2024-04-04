e528fc010648e13dad36b78f3d0dbd50420e61c8
root <root@ElOru2>
2024-04-04 15:42:42 -0300

    allegedly finished slave.c

--- slave.c
+++ slave.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "utils.h"
 #include "pipes.h"
 
@@ -7,7 +8,9 @@ int main(){
     
     // necesito recibir el path por un pipe
     char* path;
-    char* md5 = "md5sum %s";
+    char* start_command = "md5sum %s";
+    char command[MAX_PATH_SIZE - 2 + strlen(start_command)];
+    char md5[MAX_MD5_SIZE + 1];     // ojo con el offset
 
     // aca recibo el path por un pipe
 
@@ -15,10 +18,28 @@ int main(){
 
     while(continue_reading == 1){
         continue_reading = pipe_read(FD_READ, path);    // errors are handled by wrappers
-        
+
+        // fijarse que no se rompa aca
     }
 
+    sprintf(command, start_command, path);                        // store "md5sum + path" in command
 
+    FILE *fp = popen(command, "r");                     // r means read
+
+    if(fp == NULL){
+        perror("popen error");
+        exit(EXIT_FAILURE);
+    }
+
+    fgets(md5, MAX_MD5_SIZE, fp);   
+
+    // now md5 has the md5 we want the function to return
+
+    md5[MAX_MD5_SIZE+1] = '\0';
+
+    pclose(fp);
+
+    pipe_write(FD_WRITE, md5);
 
 
 }
