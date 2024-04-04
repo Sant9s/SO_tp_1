283ac3e1ad6b88ea2d954a2e47a5304f309b24b2
MatiSapino <msapino@itba.edu.ar>
2024-04-04 14:43:48 -0300

    read and write functions working

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Primer Trabajo Practico de Sistemas Operativos
 
 prueba del git
+
+
+english
+snake_case
+if (value != 0) {
+    return 0;
+}
---
+++ pipes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int read_pipe(int fd, char buff[]) {
+    int i = 0;
+    char read_char[1] = {1};
+
+    while (read_char[0] != 0 && i < strlen(buff)) {
+        int read_result  = read(fd, read_char, 1);
+
+        if (read_result == -1) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+
+        buff[i++] = read_char[0];
+    }
+
+    buff[i] = 0;
+
+    return i;
+}
+
+int write_pipe(int fd, const char *buff) {
+
+    int write_result = write(fd, buff, strlen(buff) + 1);
+
+    if (write_result == -1) {
+        perror("write");
+        exit(EXIT_FAILURE);
+    }
+
+    return write_result;
+}
